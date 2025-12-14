de182731ec7bfadc09a707ef8977a27ab008a6fd
Giovanna Regina <giovanna.regina2005@gmail.com>
2025-12-14 16:45:04 +0000

    att

---
+++ ui/DecodificadorResistor.java
@@ -0,0 +1,11 @@
+package ui;
+
+import java.calculo.CalculoResistencia;
+import java.formatador.FormatadorResistencia;
+
+public class DecodificadorResistor {
+
+                                                         //validacao
+
+
+}
